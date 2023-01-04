e8099a44fb26ac2b0c8db67ff737cb89d2a44ad7
Sean Sundberg <seansund@us.ibm.com>
2023-01-04 09:14:49 -0600

    Adds fret board label graphql service
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/services/fret-board-labels/index.ts
+++ src/services/fret-board-labels/index.ts
@@ -1,7 +1,7 @@
 import {FretBoardLabelsApi} from './fret-board-labels.api';
-import {FretBoardLabelsMock} from './fret-board-labels.mock';
+import {FretBoardLabelsGraphql} from "./fret-board-labels.graphql";
 
 export * from './fret-board-labels.api'
 
 let _instance: FretBoardLabelsApi;
-export const getFretLabelsApi = () => _instance ? _instance : _instance = new FretBoardLabelsMock();
+export const getFretLabelsApi = () => _instance ? _instance : _instance = new FretBoardLabelsGraphql();
